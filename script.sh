3d905123ec72b64a4f35315d0402db2c4a326293
sureshgdd <108092452+sureshgdd@users.noreply.github.com>
2022-11-04 16:19:48 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 docker build -t webapp .
 sleep 10
-docker run -d --name web_appl -p 8081:8080 webapp
+docker run -d --name web_appl -p 8081:8080 --mount source=$1,destination=/opt webapp
